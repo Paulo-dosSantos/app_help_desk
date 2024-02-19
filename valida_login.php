7044c3a3961b4bdc068736edc5e1408b87146a2e
Paulo Henrique <paulo.13.henrique800@gmail.com>
2024-02-18 22:39:29 -0300

    autenticando usuário

--- valida_login.php
+++ valida_login.php
@@ -1,5 +1,32 @@
 <?php   
 
+//variavel que identifica se a autenticação foi feita
+$usuario_autenticado=false;
+$usuarios_app= array(array('email'=>'admteste@gmail.com.br','senha'=>"1234567"),
+array('email'=>'admteste@gmail.com.br','senha'=>"1234567"));
+
+
+
+
+
+
+foreach($usuarios_app as $user){
+ if($user['email']==$_POST['email'] && $user['senha']==$_POST['senha']){
+    $usuario_autenticado=true;
+ }
+}
+
+if($usuario_autenticado){
+    echo 'usuário autenticado';
+}
+else{
+   header("Location: index.php?login=erro");
+}
+
+/*
+echo '</pre>';
+print_r($usuarios_app);
+echo '</pre>';
 
     print_r($_POST);
 
@@ -8,7 +35,7 @@
     echo $_POST['senha'];
 
   
-
+*/
 
  
 ?>
